50c17d7fd7b6d214744c23829b827670ab495440
Romeu Gomes <oh4ianon@hotmail.com>
2016-11-30 21:37:38 +0000

    Add initial encryption source.

---
+++ EncryptionModule/src/pt/a21240332/PMCrypto.java
@@ -0,0 +1,172 @@
+package pt.a21240332;
+
+import org.bouncycastle.crypto.digests.WhirlpoolDigest;
+import org.bouncycastle.util.encoders.Base64;
+
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+
+import static javax.xml.bind.DatatypeConverter.printHexBinary;
+
+/**
+ * The PMCrypto class is a module of Password Manager
+ * that is used for the encryption and decryption of
+ * password and the hashing of the master password.
+ */
+public class PMCrypto {
+
+    // IV Size
+    final private static int IV_BLOCK_SIZE = 16;
+    // Whirlpool size in bytes
+    final private static int WHIRLPOOL_BLOCK_SIZE = 64;
+
+    /**
+     * This static method generates an Initialization Value
+     * with 16 bytes of size. It is used in the AES encryption
+     * of the passwords.
+     *
+     * @return String The IV to be used in encryption.
+     */
+    public static String generateIV() {
+        String alphanumeric = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        SecureRandom rnd = new SecureRandom();
+
+        StringBuilder sb = new StringBuilder(IV_BLOCK_SIZE);
+        for (int i = 0; i < IV_BLOCK_SIZE; i++)
+            sb.append(alphanumeric.charAt(rnd.nextInt(alphanumeric.length())));
+        return sb.toString();
+    }
+
+    /**
+     * This static method generates a salt with 8 bytes
+     * of size. It will be used in the AES encryption of
+     * the passwords.
+     *
+     * @return byte[] The salt to be used in encryption.
+     */
+    public static byte[] generateSalt() {
+        byte[] bytes = new byte[8];
+
+        try {
+            SecureRandom.getInstanceStrong().nextBytes(bytes);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return bytes;
+    }
+
+    /**
+     * This static method encrypts the text together
+     * with the salt, with the help of IV. After the
+     * encryption, it encodes the bytes in Base64 to
+     * get a readable string.
+     *
+     * @param text Plain text password.
+     * @param salt Salt to be used.
+     * @param aesiv IV to be used.
+     * @return String
+     */
+    public static String AESEncryptPBKDF2(String text, byte[] salt, String aesiv) {
+        try {
+            // IV construction
+            IvParameterSpec iv = new IvParameterSpec(aesiv.getBytes("UTF-8"));
+
+            // Creation of the key
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(text.toCharArray(), salt, 65536, 256);
+            SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+
+            // Actual encryption
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, secret, iv);
+
+            byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+
+            return Base64.toBase64String(encrypted);
+
+        } catch (NoSuchAlgorithmException |
+                InvalidKeyException |
+                NoSuchPaddingException |
+                BadPaddingException |
+                UnsupportedEncodingException |
+                InvalidKeySpecException |
+                InvalidAlgorithmParameterException |
+                IllegalBlockSizeException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * This static method decrypts the encrypted
+     * password encoded in Base64. Like the encryption
+     * it needs the IV, salt and the original text.
+     *
+     * @param encrypted Encrypted password encoded in Base64.
+     * @param original Original plain text password.
+     * @param salt Salt used in the encryption.
+     * @param aesiv IV used in the encryption.
+     * @return String
+     */
+    public static String AESDecryptPBKDF2(String encrypted, String original, byte[] salt, String aesiv) {
+        try {
+            // IV construction
+            IvParameterSpec iv = new IvParameterSpec(aesiv.getBytes("UTF-8"));
+
+            // Creation of the key
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(original.toCharArray(), salt, 65536, 256);
+            SecretKey secret = new SecretKeySpec(factory.generateSecret(spec).getEncoded(), "AES");
+
+            // Actual encryption
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secret, iv);
+
+            byte[] decrypted = cipher.doFinal(Base64.decode(encrypted));
+
+            return new String(decrypted);
+
+        } catch (NoSuchAlgorithmException |
+                InvalidKeyException |
+                NoSuchPaddingException |
+                BadPaddingException |
+                InvalidKeySpecException |
+                InvalidAlgorithmParameterException |
+                UnsupportedEncodingException |
+                IllegalBlockSizeException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * This static method hashes the given
+     * password using the Whirlpool Hashing
+     * Algorithm. This algorithm is 512 bits
+     * long.
+     *
+     * @param password Password to hash.
+     * @return String
+     */
+    public static String whirlpoolDigest(byte[] password) {
+        byte result[] = new byte[WHIRLPOOL_BLOCK_SIZE];
+
+        // Hashing
+        WhirlpoolDigest wpHash = new WhirlpoolDigest();
+        wpHash.update(password, 0, password.length);
+        wpHash.doFinal(result, 0);
+
+        return printHexBinary(result);
+    }
+}
---
+++ EncryptionModule/src/test/pt/a21240332/CryptoJUnit.java
@@ -0,0 +1,201 @@
+package pt.a21240332;
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CryptoJUnit extends TestCase {
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        System.out.println("Starting tests...");
+    }
+
+    @Test
+    public void testWhirlpoolHashVeryShort() {
+        String str = "If";
+        String expected = "0e8f0872fc85b6ac60a9915de65b911ec48c76932d11ea235bab8e6e0035e998564ef815cb241240daa673e3576ca237cbebd3be41e4f4aaef7f349ade3507e1".toUpperCase();
+
+        System.out.println("---- TESTING testWhirlpoolHashVeryShort() ----");
+        System.out.println("String: " + str);
+        System.out.println("Hash: " + expected);
+
+        Assert.assertEquals(PMCrypto.whirlpoolDigest(str.getBytes()), expected);
+
+        System.out.println("Success!\n");
+    }
+
+    @Test
+    public void testWhirlpoolHashShort() {
+        String str = "If Pickford's packers";
+        String expected = "b73d38e35ab95e3716b3b3710f49d717e658a365bf371e2c23b36b1ffd998a76242ca812310f831f61d550995dd9a1f8017444b0b43fc0eb26ec24af2e1f8ecc".toUpperCase();
+
+        System.out.println("---- TESTING testWhirlpoolHashShort() ----");
+        System.out.println("String: " + str);
+        System.out.println("Hash: " + expected);
+
+        Assert.assertEquals(PMCrypto.whirlpoolDigest(str.getBytes()), expected);
+
+        System.out.println("Success!\n");
+    }
+
+    @Test
+    public void testWhirlpoolHashMedium() {
+        String str = "If Pickford's packers packed a packet";
+        String expected = "bbbe58672da767b54d455085384ef0d9d302f6838e06bfbcc54823a05f40917979b0c0625a858274851245206fe000d98c9d39ada617589bdd2d42ffa630c6fb".toUpperCase();
+
+        System.out.println("---- TESTING testWhirlpoolHashMedium() ----");
+        System.out.println("String: " + str);
+        System.out.println("Hash: " + expected);
+
+        Assert.assertEquals(PMCrypto.whirlpoolDigest(str.getBytes()), expected);
+
+        System.out.println("Success!\n");
+    }
+
+    @Test
+    public void testWhirlpoolHashLarge() {
+        String str = "If Pickford's packers packed a packet of crisps would the packet of crisps";
+        String expected = "4fcc15d1e767f6bf4563ad78d1bb4ec15f49a113a3d5017a08c38666b0c29cba463ece963f5e5ecfb5e46a51a1989a3b2163a28ccad1c832e28788f9edf90e41".toUpperCase();
+
+        System.out.println("---- TESTING testWhirlpoolHashLarge() ----");
+        System.out.println("String: " + str);
+        System.out.println("Hash: " + expected);
+
+        Assert.assertEquals(PMCrypto.whirlpoolDigest(str.getBytes()), expected);
+
+        System.out.println("Success!\n");
+    }
+
+    @Test
+    public void testWhirlpoolHashVeryLarge() {
+        String str = "If Pickford's packers packed a packet of crisps would the packet of crisps that Pickford's packers packed survive for two and a half years?";
+        String expected = "f55a3a1f3319c57cbaaf213e5d0390e9f8fe0e3f8e0cfd74c76e99f9b8e3d2020b2ea68c9cea76833ec92e567c4e358d9ae1bc3d1a4facf0eb115497439e5d58".toUpperCase();
+
+        System.out.println("---- TESTING testWhirlpoolHashVeryLarge() ----");
+        System.out.println("String: " + str);
+        System.out.println("Hash: " + expected);
+
+        Assert.assertEquals(PMCrypto.whirlpoolDigest(str.getBytes()), expected);
+
+        System.out.println("Success!\n");
+    }
+
+    @Test
+    public void testAesEncryptionVeryShort() {
+        String str = "If";
+
+        String iv = PMCrypto.generateIV();
+        byte[] salt = PMCrypto.generateSalt();
+
+        String encrypted = PMCrypto.AESEncryptPBKDF2(str, salt, iv);
+        String decrypted = PMCrypto.AESDecryptPBKDF2(encrypted, str, salt, iv);
+
+        System.out.println("---- TESTING testAesEncryptionVeryLarge() ----");
+        System.out.println("String: " + str);
+        System.out.println("IV: " + iv);
+        System.out.println("Salt: " + salt.toString());
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + decrypted);
+
+        Assert.assertEquals(decrypted, str);
+
+        System.out.println("Success!\n");
+    }
+
+    @Test
+    public void testAesEncryptionShort() {
+        String str = "If Pickford's packers";
+
+        String iv = PMCrypto.generateIV();
+        byte[] salt = PMCrypto.generateSalt();
+
+        String encrypted = PMCrypto.AESEncryptPBKDF2(str, salt, iv);
+        String decrypted = PMCrypto.AESDecryptPBKDF2(encrypted, str, salt, iv);
+
+        System.out.println("---- TESTING testAesEncryptionVeryLarge() ----");
+        System.out.println("String: " + str);
+        System.out.println("IV: " + iv);
+        System.out.println("Salt: " + salt.toString());
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + decrypted);
+
+        Assert.assertEquals(decrypted, str);
+
+        System.out.println("Success!\n");
+    }
+
+    @Test
+    public void testAesEncryptionMedium() {
+        String str = "If Pickford's packers packed a packet";
+
+        String iv = PMCrypto.generateIV();
+        byte[] salt = PMCrypto.generateSalt();
+
+        String encrypted = PMCrypto.AESEncryptPBKDF2(str, salt, iv);
+        String decrypted = PMCrypto.AESDecryptPBKDF2(encrypted, str, salt, iv);
+
+        System.out.println("---- TESTING testAesEncryptionVeryLarge() ----");
+        System.out.println("String: " + str);
+        System.out.println("IV: " + iv);
+        System.out.println("Salt: " + salt.toString());
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + decrypted);
+
+        Assert.assertEquals(decrypted, str);
+
+        System.out.println("Success!\n");
+    }
+
+    @Test
+    public void testAesEncryptionLarge() {
+        String str = "If Pickford's packers packed a packet of crisps would the packet of crisps";
+
+        String iv = PMCrypto.generateIV();
+        byte[] salt = PMCrypto.generateSalt();
+
+        String encrypted = PMCrypto.AESEncryptPBKDF2(str, salt, iv);
+        String decrypted = PMCrypto.AESDecryptPBKDF2(encrypted, str, salt, iv);
+
+        System.out.println("---- TESTING testAesEncryptionVeryLarge() ----");
+        System.out.println("String: " + str);
+        System.out.println("IV: " + iv);
+        System.out.println("Salt: " + salt.toString());
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + decrypted);
+
+        Assert.assertEquals(decrypted, str);
+
+        System.out.println("Success!\n");
+    }
+
+    @Test
+    public void testAesEncryptionVeryLarge() {
+        String str = "If Pickford's packers packed a packet of crisps would the packet of crisps that Pickford's packers packed survive for two and a half years?";
+
+        String iv = PMCrypto.generateIV();
+        byte[] salt = PMCrypto.generateSalt();
+
+        String encrypted = PMCrypto.AESEncryptPBKDF2(str, salt, iv);
+        String decrypted = PMCrypto.AESDecryptPBKDF2(encrypted, str, salt, iv);
+
+        System.out.println("---- TESTING testAesEncryptionVeryLarge() ----");
+        System.out.println("String: " + str);
+        System.out.println("IV: " + iv);
+        System.out.println("Salt: " + salt.toString());
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + decrypted);
+
+        Assert.assertEquals(decrypted, str);
+
+        System.out.println("Success!\n");
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+}
